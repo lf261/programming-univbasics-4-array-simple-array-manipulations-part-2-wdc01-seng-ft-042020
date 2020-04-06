9fb9ed8e371607e6b90d75f82344a15462b05314
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-06 16:33:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,4 +11,13 @@ def using_unique(array)
 end
 
 def using_flatten(array)
-  
+  array.flatten
+end 
+
+def using_delete(array, string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, int)
+  array.delete_at(int)
+end 
